188c30261332fadc4199926420faeed22dee226d
Daniel Moreira <danieljt84@gmail.com>
2021-01-04 21:05:39 -0300

    Framework Lombok adicionada

---
+++ backend/src/main/java/com/danielmoreira/dsdelivery/model/Produto.java
@@ -0,0 +1,10 @@
+package com.danielmoreira.dsdelivery.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String Descricao;
+	private String imagemUri;
+	private Double preco;
+}
